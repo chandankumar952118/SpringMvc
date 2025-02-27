34459862d50322fc123bdbc25734f2240df71113
chandanchand1107 <85780336+chandankumar952118@users.noreply.github.com>
2025-02-27 19:47:36 +0530

    Mvc

---
+++ springwebmvc/src/main/java/com/springboot/web/SpringwebmvcApplication.java
@@ -0,0 +1,14 @@
+package com.springboot.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebmvcApplication.class, args);
+		System.out.println("Hello boot");
+	}
+
+}
